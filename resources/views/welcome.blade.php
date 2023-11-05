c68c52c2c57de11d9dd2842e5925f9e751e47ee7
tzelleke <theodros.zelleke@gmail.com>
2023-11-05 17:36:34 +0000

    feat: adds layout component

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <title>Laravel</title>
-
-  @vite(['resources/scss/app.scss', 'resources/js/app.js'])
-</head>
-
-<body>
+<x-layout>
   <main class="container">
-    <h1 class="display-4">Hello, Laravel + Bootstrap!</h1>
+    <h1 class="display-6">Hello Laravel + Bootstrap!</h1>
     <p>Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})</p>
   </main>
-</body>
-
-</html>
+</x-layout>
