c4f026dce0d39a773716bd02a348086041997c14
tzelleke <theodros.zelleke@gmail.com>
2023-11-07 21:40:18 +0100

    adds bootstrap navbar with social links

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,58 @@
+<nav
+  class="navbar sticky-top navbar-expand-lg bg-primary"
+  data-bs-theme="dark"
+>
+  <div class="container">
+    <a
+      class="navbar-brand"
+      href="#"
+    ><strong>Theodros Zelleke</strong>&nbsp;&nbsp;Laravel Developer Profile</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div
+      class="collapse navbar-collapse"
+      id="navbarSupportedContent"
+    >
+      <div class="d-flex gap-2 flex-wrap socials ms-auto">
+        <ul class="nav d-flex">
+          <li class="nav-item">
+            <a
+              class="nav-link py-1 px-2"
+              href="https://github.com/tzelleke"
+              target="_blank"
+              rel="noopener"
+              aria-label="Open My Github Profile"
+            >@include('icons.github')</a>
+          </li>
+          <li class="nav-item">
+            <a
+              class="nav-link py-1 px-2"
+              href="https://stackoverflow.com/users/1438416/theodros-zelleke"
+              target="_blank"
+              rel="noopener"
+              aria-label="Open My StackOverflow Profile"
+            >@include('icons.stack-overflow')</a>
+          </li>
+          <li class="nav-item">
+            <a
+              class="nav-link py-1 px-2"
+              href="https://stackoverflow.com/users/1438416/theodros-zelleke"
+              target="_blank"
+              rel="noopener"
+              aria-label="Open My StackOverflow Profile"
+            >@include('icons.linkedin')</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>
