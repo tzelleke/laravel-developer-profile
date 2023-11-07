f12a1bafa77e8d3757a527c5a191ee4437ebe504
tzelleke <theodros.zelleke@gmail.com>
2023-11-07 21:37:16 +0100

    loads bootstrap collapse js module

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -5,8 +5,9 @@
  */
 
 import axios from 'axios';
-window.axios = axios;
+import {Collapse} from 'bootstrap';
 
+window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
