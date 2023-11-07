08c346ffb3b232da26e2820c135bdd139ebfea8b
tzelleke <theodros.zelleke@gmail.com>
2023-11-07 21:51:15 +0100

    bs badge component

---
+++ packages/bootstrap-components/resources/views/components/badge.blade.php
@@ -0,0 +1,14 @@
+@props([
+    'label' => null,
+    'color' => 'primary',
+])
+
+@php
+  $attributes = $attributes->class(['badge bg-' . $color])->merge([
+      //
+  ]);
+@endphp
+
+<span {{ $attributes }}>
+  {{ $label ?? $slot }}
+</span>
